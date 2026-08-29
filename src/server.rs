d66f124dd2060e589854fd012b18cbc751fdbfa9
Miguel Loranca <mloranca703@gmail.com>
2026-08-28 22:30:25 -0600

    changed the port

--- src/server.rs
+++ src/server.rs
@@ -2,7 +2,7 @@ use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80")?;
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
 
     for stream in listener.incoming() {
         let mut stream = stream?;
