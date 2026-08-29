9c92601246fa6c7c7f9c7bcb7002e8a09d9c982c
Miguel Loranca <mloranca703@gmail.com>
2026-08-28 22:33:32 -0600

    added the server

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,14 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
-fn main() {}
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
+    stream.write(b"Hello from the client")?;
+
+    let mut buff = [0; 512];
+    stream.read(&mut buff)?;
+
+    println!("Recieved: {}", String::from_utf8_lossy(&buff));
+
+    Ok(())
+}
