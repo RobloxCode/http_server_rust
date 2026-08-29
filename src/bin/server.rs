ce74526badf2239d957ad0e7f7ae32834d3245da
Miguel Loranca <mloranca703@gmail.com>
2026-08-28 22:44:09 -0600

    added the Server struct

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,15 +1,28 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+struct Server;
 
-    for stream in listener.incoming() {
-        let mut stream = stream?;
-        let mut buff = [0; 512];
-        stream.read(&mut buff)?;
-        stream.write(b"Hello from server")?;
+impl Server {
+    fn start(port: &str) -> std::io::Result<()> {
+        let mut pattern = "127.0.0.1:".to_string();
+        pattern.push_str(&port);
+
+        let listener = TcpListener::bind(pattern)?;
+
+        for stream in listener.incoming() {
+            let mut stream = stream?;
+            let mut buff = [0; 512];
+            stream.read(&mut buff)?;
+            stream.write(b"Hello from server")?;
+        }
+
+        Ok(())
     }
+}
+
+fn main() -> std::io::Result<()> {
+    Server::start("8080")?;
 
     Ok(())
 }
