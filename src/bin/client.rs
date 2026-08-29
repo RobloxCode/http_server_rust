d1ed7d67cfa4c9040a09a99eacfc83274e65c725
Miguel Loranca <mloranca703@gmail.com>
2026-08-28 22:48:20 -0600

    added client struct

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,14 +1,28 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
+struct Client;
+
+impl Client {
+    fn request(port: &str) -> std::io::Result<()> {
+        let mut pattern = "127.0.0.1:".to_string();
+        pattern.push_str(&port);
+
+        let mut stream = TcpStream::connect(&pattern)?;
+
+        stream.write(b"Hello from the client")?;
+
+        let mut buff = [0; 512];
+        stream.read(&mut buff)?;
+
+        println!("Recieved: {}", String::from_utf8_lossy(&buff));
+
+        Ok(())
+    }
+}
+
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
-    stream.write(b"Hello from the client")?;
-
-    let mut buff = [0; 512];
-    stream.read(&mut buff)?;
-
-    println!("Recieved: {}", String::from_utf8_lossy(&buff));
+    Client::request("8080")?;
 
     Ok(())
 }
